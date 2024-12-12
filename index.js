44a1761df06dcc12d385f2b8913e7c44eb8da48f
Anthony Van de Gejuchte <anthonyvdgent@gmail.com>
2024-12-12 19:42:55 +0100

    Get the current file/dir location more reliable

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import { join as pathJoin, dirname } from 'path';
 import { boot } from '@mighty-multitask-server/boot';
 
-if (import.meta.url !== `file:///${ process.argv[1].replace(/\\/g, '/').replace(/^\/+/, '')}`) {
+if (import.meta.filename !== process.argv[1]) {
     console.error('This file should only be used as the main entry point');
     process.exit(1);
 }
-
-boot(pathJoin(dirname(import.meta.url.replace(/^file:\/+/, '/')), 'src/app'));
+boot(pathJoin(import.meta.dirname, 'src/app'));
