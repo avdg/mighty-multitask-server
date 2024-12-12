c4b4d82e7da4e1b7e0f26fe9e1e661a12a4baaa9
Anthony Van de Gejuchte <anthony.van.de.gejuchte@studioemma.com>
2024-12-12 07:30:07 +0100

    Fix file match condition when checking if the current file is the main entry point

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { join as pathJoin, dirname } from 'path';
 import { boot } from '@mighty-multitask-server/boot';
 
-if (import.meta.url !== `file:///${ process.argv[1].replace(/\\/g, '/')}`) {
+if (import.meta.url !== `file:///${ process.argv[1].replace(/\\/g, '/').replace(/^\/+/, '')}`) {
     console.error('This file should only be used as the main entry point');
     process.exit(1);
 }
