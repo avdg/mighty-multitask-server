04d8db78cfcb26bea535bd8641087f1c29097531
Anthony Van de Gejuchte <anthonyvdgent@gmail.com>
2024-12-13 21:32:57 +0100

    Add modules loader

--- src/app/config/config.js
+++ src/app/config/config.js
@@ -1,6 +1,13 @@
+import { join as pathJoin } from 'path';
+
 export default {
+    appModules: {
+        'appPaths': [
+            pathJoin(import.meta.dirname, '../code'),
+        ]
+    },
     webserver: {
         hostname: 'localhost',
         port: 43211,
-    }
+    },
 }
